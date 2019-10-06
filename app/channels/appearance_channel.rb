677b446ce1745032e3289120f483110b8396e562
Kirill Ilyin <cuurjol@gmail.com>
2019-10-06 22:43:38 +0300

    Refactored AppearanceChannel

--- app/channels/appearance_channel.rb
+++ app/channels/appearance_channel.rb
@@ -5,7 +5,6 @@ class AppearanceChannel < ApplicationCable::Channel
   end
 
   def unsubscribed
-    stream_from 'appearance_channel'
     broadcast_online_users(false)
   end
 
@@ -13,6 +12,6 @@ class AppearanceChannel < ApplicationCable::Channel
 
   def broadcast_online_users(online)
     current_user.update_attribute(:online, online)
-    ActionCable.server.broadcast('appearance_channel', users: User.online.as_json(only: %i[nickname]))
+    ActionCable.server.broadcast('appearance_channel', users: User.online.as_json(only: :nickname))
   end
 end
