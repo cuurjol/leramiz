b430fbd019d4a01c748e69b7a3bdd7dbbd19277f
Kirill Ilyin <cuurjol@gmail.com>
2019-10-06 11:27:24 +0300

    Refactored room.js file

--- app/assets/javascripts/channels/room.js
+++ app/assets/javascripts/channels/room.js
@@ -1,6 +1,33 @@
 // https://decaffeinate-project.org/
 // http://js2.coffee/
 
+let createRoomChannel = function(roomId) {
+  return App.room = App.cable.subscriptions.create({channel: "RoomChannel", roomId}, {
+    connected() {
+      return console.log('Connected to RoomChannel');
+    },
+
+    disconnected() {
+      return console.log('Disconnected from RoomChannel');
+    },
+
+    received(data) {
+      console.log('Received message: ' + data['message']);
+      $('#messages').append(data['message']);
+
+      return scroll_bottom();
+    },
+
+    speak(message) {
+      return this.perform('speak', {message});
+    }
+  });
+};
+
+let scroll_bottom = function () {
+  messages.scrollTop = messages.scrollHeight;
+};
+
 jQuery(document).on('turbolinks:load', function() {
   const messages = $('#messages');
 
@@ -21,26 +48,3 @@ jQuery(document).on('turbolinks:load', function() {
     }
   });
 });
-
-var createRoomChannel = function(roomId) {
-  return App.room = App.cable.subscriptions.create({channel: "RoomChannel", roomId}, {
-    connected() {
-      return console.log('Connected to RoomChannel');
-    },
-
-    disconnected() {
-      return console.log('Disconnected from RoomChannel');
-    },
-
-    received(data) {
-      console.log('Received message: ' + data['message']);
-      $('#messages').append(data['message']);
-
-      return $(messages).animate({scrollTop: 20000000}, "slow");
-    },
-
-    speak(message) {
-      return this.perform('speak', {message});
-    }
-  });
-};
