af8adaed71eb8f76fe44812b7a4b511d68b551fd
Kirill Ilyin <cuurjol@gmail.com>
2019-10-23 20:58:09 +0300

    Replace text_field_tag with text_area_tag and add some features

--- app/assets/javascripts/channels/room.js
+++ app/assets/javascripts/channels/room.js
@@ -23,8 +23,23 @@ let scroll_bottom = function () {
   messages.scrollTop = messages.scrollHeight;
 };
 
+// https://stackoverflow.com/questions/44433953/jumping-scrollbar-after-auto-expanding-textarea
+let resizeTextarea = function(elem) {
+  // two additional variables getting the top and left scroll positions.
+  let scrollLeft = window.pageXOffset;
+  let scrollTop  = window.pageYOffset;
+
+  elem.css('height', 'auto').css('height', elem.prop('scrollHeight'));
+  // Applying previous top and left scroll position after textarea resize.
+  window.scrollTo(scrollLeft, scrollTop);
+};
+
 jQuery(document).on('turbolinks:load', function() {
   const messages = $('#messages');
+
+  $('textarea').on('input', function() {
+    resizeTextarea($(this));
+  });
 
   if (messages.length > 0) {
     createRoomChannel(messages.data('room-id'));
@@ -37,13 +52,27 @@ jQuery(document).on('turbolinks:load', function() {
   }
 
   return $(document).on('keypress', '#message_body', function(event) {
-    const message = event.target.value;
-    if (event.keyCode === 13) {
+    let message = event.target.value;
+
+    if ((event.shiftKey || event.ctrlKey) && (event.keyCode === 13 || event.keyCode === 10)) {
+      if (message.trim() !== '') {
+        let start = this.selectionStart;
+        event.target.value = message.substring(0, start) + "\n" + this.value.substring(start);
+        resizeTextarea($('textarea'));
+        this.selectionStart = this.selectionEnd = start + 1;
+        return event.preventDefault();
+      } else {
+        event.target.value = "";
+        return event.preventDefault();
+      }
+    } else if (event.keyCode === 13) {
       if (message.trim() !== '') {
         App.room.speak(message);
         event.target.value = "";
+        resizeTextarea($('textarea'));
         return event.preventDefault();
       } else {
+        event.target.value = "";
         return event.preventDefault();
       }
     }
