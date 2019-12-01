e279961b8b0931554c43139e893b4a8b2556308c
Kirill Ilyin <cuurjol@gmail.com>
2019-12-01 17:41:18 +0300

    Update resque.rb

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,2 +1,9 @@
+if Rails.env.development?
+  Resque.redis = Redis.new(host: 'localhost', port: '6379')
+else
+  REDIS = Redis.new(url: ENV['REDISTOGO_URL'])
+  Resque.redis = REDIS
+end
+
 Resque.logger = Logger.new(Rails.root.join('log', "#{Rails.env}_resque.log"))
 Resque.logger.level = Logger::DEBUG
